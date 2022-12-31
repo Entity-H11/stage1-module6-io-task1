1accc3b9e05b34d22b43e62604d02221ed80740c
emmanuel hernandez <emmanuelhern00@gmail.com>
2022-12-31 18:18:51 -0400

    Refactoring from Quality report

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -6,24 +6,28 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class FileReader {
 
+    Logger logger = Logger.getLogger(FileReader.class.getName());
     public Profile getDataFromFile(File file) {
 
 
-        Map<String, String> profileProperties = new HashMap<String, String>();
-        String property = "", value = "";
+        Map<String, String> profileProperties = new HashMap<>();
+        StringBuilder property = new StringBuilder();
+        StringBuilder value = new StringBuilder();
 
         try (FileInputStream input = new FileInputStream(file)) {
             boolean isProperty = false;
             int ch;
             while ((ch = input.read()) != -1) {
                 if (ch == '\n') {
-                    profileProperties.put(property.trim(), value.trim());
-                    property = "";
-                    value = "";
+                    profileProperties.put(property.toString().trim(), value.toString().trim());
+                    property = new StringBuilder();
+                    value = new StringBuilder();
                     isProperty = false;
                 }
 
@@ -32,20 +36,21 @@ public class FileReader {
                     continue;
                 }
 
-                if (isProperty) value += (char) ch;
-                else property += (char) ch;
+                if (isProperty) value.append((char) ch);
+                else property.append((char) ch);
 
             }
 
         } catch (FileNotFoundException e) {
-            System.err.println(e);
-        } catch (IOException e) {
-            System.err.println(e);
+            logger.log(Level.SEVERE, "File Not Found, ", e);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
         }
 
         return parsingFileIntoMap(profileProperties);
     }
-    
+
     public Profile parsingFileIntoMap(Map<String, String> properties) {
         return new Profile(properties.get("Name").trim(),
                 Integer.valueOf(properties.get("Age")),
