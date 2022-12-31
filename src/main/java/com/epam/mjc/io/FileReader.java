de880618b66cd8c3cb5b6644d6e44d9e07474f64
emmanuel hernandez <emmanuelhern00@gmail.com>
2022-12-31 17:11:35 -0400

    parsing file into Profile class

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,57 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+
+        Map<String, String> profileProperties = new HashMap<String, String>();
+        String property = "", value = "";
+
+        try (FileInputStream input = new FileInputStream(file)) {
+            boolean isProperty = false;
+            int ch;
+            while ((ch = input.read()) != -1) {
+                if (ch == '\n') {
+                    profileProperties.put(property.trim(), value.trim());
+                    property = "";
+                    value = "";
+                    isProperty = false;
+                }
+
+                if (ch == ':' && !isProperty) {
+                    isProperty = true;
+                    continue;
+                }
+
+                if (isProperty) value += (char) ch;
+                else property += (char) ch;
+
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println(e);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+
+        return parsingFileIntoMap(profileProperties);
     }
+    
+    public Profile parsingFileIntoMap(Map<String, String> properties) {
+        return new Profile(properties.get("Name").trim(),
+                Integer.valueOf(properties.get("Age")),
+                properties.get("Email"),
+                Long.valueOf(properties.get("Phone"))
+        );
+    }
+
 }
